929ba952d240166037096bde7f552e3e69aba9df
aishamuratnazarova <>
2020-05-26 14:44:10 +0300

    finally

--- spa/src/AboutMe.js
+++ spa/src/AboutMe.js
@@ -1,4 +1,5 @@
 export default function addName() {
     let elem = document.getElementById("app")
-    elem.innerHTML = "Айша Муратназарова M3306";
+    elem.innerHTML = "Айша Муратназарова  M3306";
+
 }
